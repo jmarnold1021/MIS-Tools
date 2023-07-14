c7cde3b701ce1539824e8f57573a9a659b04e353
Jared Arnold-MISAdmin <admin_ja@ltccdomain.local>
2023-07-14 16:24:37 -0700

    Update mis_rpt_backups.ps1
    * Now backups up default schema values
    * Cleans backups older than RENTENTION_DAYS

--- bin/mis_rpt_backup.ps1
+++ bin/mis_rpt_backup.ps1
@@ -16,6 +16,8 @@
 $PACKAGE_ROOT = $PSScriptRoot + '\..'
 cd $PACKAGE_ROOT
 
+$RETENTION_DAYS = 60
+
 $log_file = "$PACKAGE_ROOT/mistools/logs/mis_rpt_backup.log"
 
 # db server instance..
@@ -25,20 +27,18 @@ $serv = new-object ('Microsoft.SqlServer.Management.Smo.Server') "ltcc-db"
 $db = $serv.Databases["coll18_production"]
 
 $bdate = Get-Date -Format "yyMMdd"
-$bpath = "\\ltcc-app\MIS\MIS_SQL_Backups\mis_rpt_bac_" + $bdate
+$bpath = "\\ltcc-app\MIS\MIS_SQL_Backups\mis_rpt_bac_"
 
-New-Item -Path $bpath -ItemType Directory | out-null
+New-Item -Path ($bpath + $bdate) -ItemType Directory | out-null
 
-Write-Output "Starting backup on " + $bdate > $log_file
+Write-Output ("Starting backup on {0}" -f $bdate) > $log_file
 foreach ( $tables in $db.Tables ) {
 
     if ( ($tables.Name.StartsWith('CAST') -or $tables.Name.StartsWith("CAHR")) -and $tables.Name.EndsWith("RPT") )
     {
 
-        # if ($Report -ne '' -and $Report  # new need
-
         $options = new-object ('Microsoft.SqlServer.Management.Smo.ScriptingOptions')
-        $options.FileName = $bpath + "\dbo.{0}.Table.sql" -f $tables.Name
+        $options.FileName = ("{0}{1}\dbo.{2}.Table.sql" -f $bpath,$bdate,$tables.Name)
         $options.ScriptDrops  = $true
         $options.AnsiFile     = $true
         $options.NoCollation  = $true
@@ -50,27 +50,34 @@ foreach ( $tables in $db.Tables ) {
         $options.AppendToFile       = $true
         $options.IncludeIfNotExists = $true
         $options.ScriptSchema       = $true
+        $options.DriDefaults        = $true
         $options.Indexes            = $true
         Write-Output ("Writing Schemas {0}" -f $tables.Name) >> $log_file
         $tables.EnumScript($options) | out-null
 
         # once append file has been set I guess can't just turn off from old path name. unless .FileName is appending paths or someting odd.
         $options = new-object ('Microsoft.SqlServer.Management.Smo.ScriptingOptions')
-        $options.FileName = $bpath + "\dbo.{0}.Data.sql" -f $tables.Name
+        $options.FileName = ("{0}{1}\dbo.{2}.Data.sql" -f $bpath,$bdate,$tables.Name)
         $options.IncludeIfNotExists = $false
         $options.ScriptSchema       = $false
+        $options.DriDefaults        = $false
         $options.ScriptData         = $true
         Write-Output ("Writing Data {0}" -f $tables.Name) >> $log_file
         $tables.EnumScript($options) | out-null
+        break
 
     }
 }
 
-$bzip = "\\ltcc-app\MIS\MIS_SQL_Backups\mis_rpt_bac_" + $bdate + ".zip"
+$bzip = $bpath + $bdate + ".zip"
 
-Write-Output ("Archiving Data {0} to {1}" -f $bpath, $bzip) >> $log_file
-[System.IO.Compression.ZipFile]::CreateFromDirectory($bpath, $bzip)
+Write-Output ("Archiving Data {0}{1} to {2}" -f $bpath,$bdate,$bzip) >> $log_file
+[System.IO.Compression.ZipFile]::CreateFromDirectory(($bpath+ $bdate), $bzip) # the powershell compress has soe odd status banner...so use .NET one from FileSystem
 
-Remove-Item $bpath -Recurse
+Remove-Item ($bpath + $bdate) -Recurse
+
+
+Write-Output ("Cleaning Backups older than {0} days" -f $RETENTION_DAYS) >> $log_file
+Get-ChildItem -Path ($bpath + "*.zip")  | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-$RETENTION_DAYS))} | Remove-Item
 
 exit 0
